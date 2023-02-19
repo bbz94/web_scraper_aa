d3a511fa3973433fea68207b5b4311e748f44fc3
Kristians Voronics <Kristians.Voronics@if.lv>
2023-02-19 18:39:20 +0200

    Add files to git

--- webScraperAAScripts/webScraperAuto24.ps1
+++ webScraperAAScripts/webScraperAuto24.ps1
@@ -1,12 +1,12 @@
 param(
-    $rootPath = "$env:temp\autoplius",
+    $rootPath = "$env:temp\auto24",
     $telegramtoken = $(Get-AutomationVariable -Name 'telegramtoken'),
     $telegramchatid = $(Get-AutomationVariable -Name 'telegramchatid'),
     $dbPath = "$rootPath\db.txt",
-    $domain = "autoplius.lt",
-    $htmlPath = "$rootPath\report\autoplius_caravan_$((get-date).tostring('dd_MM_yyyy')).html",
-    $htmlPathMoto = "$rootPath\report\autoplius_moto_$((get-date).tostring('dd_MM_yyyy')).html",
-    $defaultHtmlPath = "$rootPath\default.html"
+    $domain = "eng.auto24.ee",
+    $htmlPath = "$rootPath\report\auto24_caravan_$((get-date).tostring('dd_MM_yyyy')).html",
+    $htmlPathMoto = "$rootPath\report\auto24_moto_$((get-date).tostring('dd_MM_yyyy')).html",
+    $defaultHtmlPath = "$rootPath\defaultAuto24.html"
 )
 
 # create temp directory
@@ -20,8 +20,8 @@ if(!(test-path -Path "$rootPath\report")){
     New-Item -Path "$rootPath\report" -ItemType Directory
 }
 
-$uri = 'https://raw.githubusercontent.com/bbz94/web_scraper_aa/main/webScraperAAScripts/defaultAutoplius.html'
-Invoke-WebRequest -Uri $uri -OutFile "$rootPath\default.html"
+$uri = 'https://raw.githubusercontent.com/bbz94/web_scraper_aa/main/webScraperAAScripts/defaultauto24.html'
+Invoke-WebRequest -Uri $uri -OutFile "$rootPath\defaultauto24.html"
 
 # functions
 Function Send-TelegramMessage {
@@ -57,7 +57,7 @@ $command = @'
     # open browser
     $Driver = Start-SeChrome -WebDriverDirectory 'C:\ProgramData\chocolatey\lib\chromedriver\tools'
     $userAgent = $driver.executescript("return navigator.userAgent;")
-    Enter-SeUrl 'https://lv.autoplius.lt/sludinajumi/autobusi-mikroautobusi-turistu-majinas' -Driver $Driver
+    Enter-SeUrl 'https://eng.auto24.ee/kasutatud/nimekiri.php?bn=2&a=113115&aj=&ae=8&af=50&otsi=search' -Driver $Driver
     $cookies = Get-SeCookie -Target $Driver
     Stop-SeDriver -Target $Driver
 '@
@@ -75,98 +75,37 @@ foreach($cookie in $cookies){
     }
 }
 
-# get pages
-# $models = 'fiat', 'ford', 'volkswagen', 'mercedes-benz'
-# $pages = @()
-# foreach ($model in $models) {
-#     $uri = "https://lv.autoplius.lt/sludinajumi/autobusi-mikroautobusi-turistu-majinas/turistu-automasinas-kemperi/$($model)?category_id=29&slist=1862616329&order_by=1&order_direction=DESC"
-#     $uri
-#     $WebRequest = Invoke-WebRequest -UseBasicParsing -Uri $uri -WebSession $session `
-#                                         -Headers @{
-#                                     "authority"="lv.autoplius.lt"
-#                                     "method"="GET"
-#                                     "path"="/sludinajumi/lietotas-automasinas?make_id=54&model_id=412"
-#                                     "scheme"="https"
-#                                     "accept"="text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-#                                     "accept-encoding"="gzip, deflate, br"
-#                                     "accept-language"="en-US,en;q=0.9"
-#                                     "cache-control"="max-age=0"
-#                                     "sec-ch-ua"="`"Opera GX`";v=`"89`", `"Chromium`";v=`"103`", `"_Not:A-Brand`";v=`"24`""
-#                                     "sec-ch-ua-mobile"="?0"
-#                                     "sec-ch-ua-platform"="`"Windows`""
-#                                     "sec-fetch-dest"="document"
-#                                     "sec-fetch-mode"="navigate"
-#                                     "sec-fetch-site"="none"
-#                                     "sec-fetch-user"="?1"
-#                                     "upgrade-insecure-requests"="1"
-#                                     };
-#     start-sleep -Seconds 2                                
-#     $pages += $WebRequest.Content
-# }
-
 $pages = ''
-$uri = 'https://lv.autoplius.lt/sludinajumi/autobusi-mikroautobusi-turistu-majinas/turistu-automasinas-kemperi?category_id=29&slist=1862616329&order_by=1&order_direction=DESC'
-$WebRequest = Invoke-WebRequest -UseBasicParsing -Uri $uri -WebSession $session `
-                                    -Headers @{
-                                "authority"="lv.autoplius.lt"
-                                "method"="GET"
-                                "path"="/sludinajumi/lietotas-automasinas?make_id=54&model_id=412"
-                                "scheme"="https"
-                                "accept"="text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-                                "accept-encoding"="gzip, deflate, br"
-                                "accept-language"="en-US,en;q=0.9"
-                                "cache-control"="max-age=0"
-                                "sec-ch-ua"="`"Opera GX`";v=`"89`", `"Chromium`";v=`"103`", `"_Not:A-Brand`";v=`"24`""
-                                "sec-ch-ua-mobile"="?0"
-                                "sec-ch-ua-platform"="`"Windows`""
-                                "sec-fetch-dest"="document"
-                                "sec-fetch-mode"="navigate"
-                                "sec-fetch-site"="none"
-                                "sec-fetch-user"="?1"
-                                "upgrade-insecure-requests"="1"
-                                };                              
-$pages + $WebRequest.Content
+$uri = 'https://eng.auto24.ee/kasutatud/nimekiri.php?bn=2&a=113115&ae=1&af=50&otsi=search&ak=0'
+$WebRequest = Invoke-WebRequest -UseBasicParsing -Uri $uri -WebSession $session                            
+$pages += $WebRequest.Content
 
 # get vans
-$models = 'Renault master', 'Fiat ducato', 'Volkswagen crafter', 'Mercedes sprinter', 'Ford transit', 'Citroen jumper', 'xc70'
+$models = 'master', 'ducato', 'crafter', 'sprinter', 'transit', 'jumper', 'xc70'
 
 foreach ($model in $models) {
-    $uri = "https://lv.autoplius.lt/sludinajumi/lietotas-automasinas?qt=$($model)&slist=1866085383&order_by=3&order_direction=DESC"
+    $uri = "https://eng.auto24.ee/kasutatud/nimekiri.php?bn=2&a=100&c=$($model)&ae=1&af=50&ssid=90862069&ak=0"
     $uri
-    $WebRequest = Invoke-WebRequest -UseBasicParsing -Uri $uri -WebSession $session `
-                                        -Headers @{
-                                    "authority"="lv.autoplius.lt"
-                                    "method"="GET"
-                                    "path"="/sludinajumi/lietotas-automasinas?make_id=54&model_id=412"
-                                    "scheme"="https"
-                                    "accept"="text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-                                    "accept-encoding"="gzip, deflate, br"
-                                    "accept-language"="en-US,en;q=0.9"
-                                    "cache-control"="max-age=0"
-                                    "sec-ch-ua"="`"Opera GX`";v=`"89`", `"Chromium`";v=`"103`", `"_Not:A-Brand`";v=`"24`""
-                                    "sec-ch-ua-mobile"="?0"
-                                    "sec-ch-ua-platform"="`"Windows`""
-                                    "sec-fetch-dest"="document"
-                                    "sec-fetch-mode"="navigate"
-                                    "sec-fetch-site"="none"
-                                    "sec-fetch-user"="?1"
-                                    "upgrade-insecure-requests"="1"
-                                    };
-    start-sleep -Seconds 2                                
+    $WebRequest = Invoke-WebRequest -UseBasicParsing -Uri $uri -WebSession $session
     $pages += $WebRequest.Content
 }
 
 # get adds
-$pattern = '<a[\n\r\s]+href="((https?:\/\/)|(\/)|(..\/))(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?.html"[\n\r\s]+class="announcement-item"[\n\r\s]+target="_blank"[\s\S]*?<\/a>'
+$pattern = '<div class="result-row item[\s\S]*?class="row-link" target="_self"><\/a>[\s\S]*?<\/div>'
 $adds = [regex]::Matches($pages, $pattern).Value
 $dbContent = get-content -Path $dbPath
 $newAdds = @()
 foreach ($add in $adds){
     # get cost
-    $patternCost = '\d*\s\d*\s\d*\&euro'
-    $price = ([regex]::Matches($add, $patternCost).value -replace ' ','') -replace '&euro'
+    $patternCost = 'price">&euro;\d*,*\d*'
+    $priceArr = [regex]::Matches($add, $patternCost).value
+    if($priceArr.count -gt 1){
+        $price = ($priceArr[-1] -replace 'price">&euro;') -replace ','
+    }else{
+        $price = ($priceArr -replace 'price">&euro;') -replace ','
+    }
     # add adds info to db
-    $pattern = '((https?:\/\/)|(\/)|(..\/))(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?.html'
+    $pattern = '\/vehicles\/\d*'
     $id = (([regex]::Matches($add, $pattern).Value) -split '-')[-1]
     if($dbContent -notcontains $id){
         $id | add-content -Path $dbPath
@@ -182,10 +121,10 @@ if($newAdds){
     # sort table 
     $htmlAdds = ($newAdds | sort price).html
     # create new file in temp directory
-    $html = ((get-content -path $defaultHtmlPath) -replace 'addsToAddHere', $htmlAdds) -replace 'src=""data-','' | out-file $htmlPath
+    $html = (get-content -path $defaultHtmlPath) -replace 'addsToAddHere', $htmlAdds | out-file $htmlPath
 
     # send telgram message
-    Send-TelegramMessage -telegramtoken $telegramtoken -telegramchatid $telegramchatid -message 'Sending autoplius caravan file:'
+    Send-TelegramMessage -telegramtoken $telegramtoken -telegramchatid $telegramchatid -message 'Sending auto24 caravan file:'
     Send-TelegramFile -telegramtoken $telegramtoken -telegramchatid $telegramchatid -filePath $htmlPath
 }else{
     write-host "Nothing to send"
@@ -196,42 +135,28 @@ $pages = @()
 $models = 'tenere', 'transalp', 'nx', 'Xt660', 'africa','KLR','Z400','KLX','CRF450','XT250','CRF300','Beta 500','Beta 390'
 
 foreach ($model in $models) {
-    $uri = "https://lv.autoplius.lt/sludinajumi/motocikli-motociklistu-apgerbs/motocikli?qt=$($model)&slist=1881749082&order_by=1&order_direction=DESC"
+    $uri = "https://eng.auto24.ee/kasutatud/nimekiri.php?bn=2&a=100&c=$($model)&ae=1&af=50&ssid=90862069&ak=0"
     $uri
-    $WebRequest = Invoke-WebRequest -UseBasicParsing -Uri $uri -WebSession $session `
-                                        -Headers @{
-                                    "authority"="lv.autoplius.lt"
-                                    "method"="GET"
-                                    "path"="/sludinajumi/lietotas-automasinas?make_id=54&model_id=412"
-                                    "scheme"="https"
-                                    "accept"="text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-                                    "accept-encoding"="gzip, deflate, br"
-                                    "accept-language"="en-US,en;q=0.9"
-                                    "cache-control"="max-age=0"
-                                    "sec-ch-ua"="`"Opera GX`";v=`"89`", `"Chromium`";v=`"103`", `"_Not:A-Brand`";v=`"24`""
-                                    "sec-ch-ua-mobile"="?0"
-                                    "sec-ch-ua-platform"="`"Windows`""
-                                    "sec-fetch-dest"="document"
-                                    "sec-fetch-mode"="navigate"
-                                    "sec-fetch-site"="none"
-                                    "sec-fetch-user"="?1"
-                                    "upgrade-insecure-requests"="1"
-                                    };
-    start-sleep -Seconds 2                                
+    $WebRequest = Invoke-WebRequest -UseBasicParsing -Uri $uri -WebSession $session
     $pages += $WebRequest.Content
 }
 
 # get adds
-$pattern = '<a[\n\r\s]+href="((https?:\/\/)|(\/)|(..\/))(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?.html"[\n\r\s]+class="announcement-item"[\n\r\s]+target="_blank"[\s\S]*?<\/a>'
+$pattern = '<div class="result-row item[\s\S]*?class="row-link" target="_self"><\/a>[\s\S]*?<\/div>'
 $adds = [regex]::Matches($pages, $pattern).Value
 $dbContent = get-content -Path $dbPath
 $newAdds = @()
 foreach ($add in $adds){
     # get cost
-    $patternCost = '\d*\s\d*\s\d*\&euro'
-    $price = ([regex]::Matches($add, $patternCost).value -replace ' ','') -replace '&euro'
+    $patternCost = 'price">&euro;\d*,*\d*'
+    $priceArr = [regex]::Matches($add, $patternCost).value
+    if($priceArr.count -gt 1){
+        $price = ($priceArr[-1] -replace 'price">&euro;') -replace ','
+    }else{
+        $price = ($priceArr -replace 'price">&euro;') -replace ','
+    }
     # add adds info to db
-    $pattern = '((https?:\/\/)|(\/)|(..\/))(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?.html'
+    $pattern = '\/vehicles\/\d*'
     $id = (([regex]::Matches($add, $pattern).Value) -split '-')[-1]
     if($dbContent -notcontains $id){
         $id | add-content -Path $dbPath
@@ -247,10 +172,10 @@ if($newAdds){
     # sort table 
     $htmlAdds = ($newAdds | sort price).html
     # create new file in temp directory
-    $html = ((get-content -path $defaultHtmlPath) -replace 'addsToAddHere', $htmlAdds) -replace 'src=""data-','' | out-file $htmlPathMoto
+    $html = (get-content -path $defaultHtmlPath) -replace 'addsToAddHere', $htmlAdds | out-file $htmlPathMoto
 
     # send telgram message
-    Send-TelegramMessage -telegramtoken $telegramtoken -telegramchatid $telegramchatid -message 'Sending autoplius moto file:'
+    Send-TelegramMessage -telegramtoken $telegramtoken -telegramchatid $telegramchatid -message 'Sending auto24 moto file:'
     Send-TelegramFile -telegramtoken $telegramtoken -telegramchatid $telegramchatid -filePath $htmlPathMoto
 }else{
     write-host "Nothing to send"
